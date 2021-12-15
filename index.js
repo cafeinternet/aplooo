4509c3adfdc011be43c3b23dd1231f680805f456
CAFE INTERNET <ubuwill@gmail.com>
2021-12-15 08:41:54 -0500

    avances

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Fondos1!
   $(document).ready(function(){
   var classCycle=[' aplo1',' aplo2'];
 
@@ -69,27 +69,64 @@ $(document).ready(function(){
    console.log(classToAdd)
 });
 
+// $(document).ready(function(){
+//   var classCycle=['/styles/fondos/001.mp4','/styles/fondos/002.mp4','/styles/fondos/003.mp4','/styles/fondos/004.mp4'];
+
+//   var randomNumber = Math.floor(Math.random() * classCycle.length);
+//   var classToAdd = classCycle[randomNumber];
+
+  
+//   var video = $("#video1");
+// video.find("source").attr("src", classToAdd);
+// video.get().load();
+// video.get().play();
+
+
+// });
+
+
+$(document).ready(function(){
+
+  var videos = [
+      [{type:'mp4', 'src':'/styles/fondos/001.mp4'}],
+      [{type:'mp4', 'src':'/styles/fondos/002.mp4'}],
+      [{type:'mp4', 'src':'/styles/fondos/003.mp4'}]
+  ];
+  
+  // selecting random item from array,you can make your own
+  var randomitem = videos[Math.floor(Math.random()*videos.length)];
+  
+  // This function adds a new video source (dynamic) in the video html tag
+  
+
+  // this function fires the video for particular video tag
+  
+  function newvideo(src)
+  {
+  var vid = document.getElementById("video1");
+  videoadd(vid,src ,'video/mp4');
+  vid.autoplay = true;
+  vid.load();
+  //vid.play();
+  }
+  // function call
+  newvideo(randomitem[0].src)
+  
+  // Added an event listener so that everytime the video finishes ,a new video is loaded from array
+  document.getElementById('myVideo').addEventListener('ended',handler,false);
+  
+  function handler(e)
+  {
+  newvideo(randomitem[0].src)
+  
+  }
+    
+  
+  })
 
 
 
-   document.addEventListener( "DOMContentLoaded", function() {
-  // it's a div, that holds your news
-  // it holds ul with news in li elements
-  var div = document.getElementById( "container" );
-  div.addEventListener( "scroll", function() {
-    var max_scroll = this.scrollHeight - this.clientHeight;
-    var current_scroll = this.scrollTop;
-    var bottom = 100;
-    if ( current_scroll + bottom >= max_scroll ) {
-      var ul = document.getElementsByTagName( "ul" )[ 0 ];
-      var current = parseInt( ul.dataset.current, 10 );
-      var li = document.getElementsByTagName( "li" )[ current ];
-      var new_li = li.cloneNode( true );
-      ul.appendChild( new_li );
-      ul.dataset.current = current + 1;
-    }
-  } );
-} );
+
 
     $.fn.shuffleChildren = function() {
   $.each(this.get(), function(index, el) {
@@ -109,44 +146,3 @@ $("#shuffle").click(function() {
   // Usage
   $(".shuffle").shuffleChildren();
 });
-
-  
-
-
-
-    // const is another way of writing var
-    // this line of code creates a variable called poetic edge which is equal to any element inside of the HTML that has a class called poetic-edge, in the case of this website each html section has this class, there are about 18 elements
-    const poeticEdges = document.getElementsByClassName("poetic-edge");
-
-    // this is called a for loop, it allows you to take a group of elements and do something which each one, this for loop is adding an event listener for a mouse click, in other words it is making sure that each poetic edge is ready to do something when a person clicks on it, in this case each poetic edge is going to call the showPoetics function when a person clicks on it
-    for (let i=0; i<poeticEdges.length; i++) {
-      poeticEdges[i].id = i;
-      poeticEdges[i].addEventListener('click', function () { showPoetics(i) });
-    }
-
-    // this is a javascript function, this particular function takes an argument, in this case the argument the letter i. arguments always go inside the parentheses of the function.
-    function showPoetics(i) {
-      // get all elements with the id of whatever i is equal to, in this case i is equal to a number 0-18
-      const thisPoeticEdge = document.getElementById(i);
-      // get all elements with the id of whatever i is equal to + 1
-      const nextPoeticEdge = document.getElementById(i+1);
-      // get the body element
-      const body = document.getElementsByTagName('body')[0];
-      // if i is equal to 0 this means that a person has clicked through the to last section of the HTML, at this point the person should be able to click back to the first section or top layer of the web page so the class time-traveling-towards-the-past is being removed and the class time-traveling-towards-the-future is being added.
-      if (i === 0) {
-        body.classList.remove('time-traveling-towards-the-past');
-        body.classList.add('time-traveling-towards-the-future');
-      }
-      // if i is equal to 18 this means that a person has clicked back through the to first section of the HTML so the class time-traveling-towards-the-future is being removed and the class time-traveling-towards-the-past is being added.
-      if (i === poeticEdges.length - 1 && body.classList.contains('time-traveling-towards-the-future')) {
-        body.classList.remove('time-traveling-towards-the-future');
-        body.classList.add('time-traveling-towards-the-past');
-      }
-      // if the body has the class time-traveling-towards-the-future then the person should be moving forward through the site so each poetic edge should be removing the class of hide, if the body doesnt have the the class time-traveling-towards-the-future then the person should be moving towards the past in which case the class of hide should be added back to the poetic edge.
-      if (body.classList.contains('time-traveling-towards-the-future')) {
-        nextPoeticEdge.classList.remove('hide');
-      } else {
-        thisPoeticEdge.classList.add('hide');
-      }
-    }
-  
