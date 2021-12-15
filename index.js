a7c071269fbee02b1c009378a872f0b0773dc33e
CAFE INTERNET <ubuwill@gmail.com>
2021-12-15 12:27:04 -0500

    ceviche atomic listo

--- index.js
+++ index.js
@@ -70,6 +70,16 @@ $(document).ready(function(){
 });
 
 // $(document).ready(function(){
+//   $("#hide").click(function(){
+//     $("p").hide();
+//   });
+//   $("#show").click(function(){
+//     $("p").show();
+//   });
+// });
+
+
+// $(document).ready(function(){
 //   var classCycle=['/styles/fondos/001.mp4','/styles/fondos/002.mp4','/styles/fondos/003.mp4','/styles/fondos/004.mp4'];
 
 //   var randomNumber = Math.floor(Math.random() * classCycle.length);
@@ -85,44 +95,44 @@ $(document).ready(function(){
 // });
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  var videos = [
-      [{type:'mp4', 'src':'/styles/fondos/001.mp4'}],
-      [{type:'mp4', 'src':'/styles/fondos/002.mp4'}],
-      [{type:'mp4', 'src':'/styles/fondos/003.mp4'}]
-  ];
+//   var videos = [
+//       [{type:'mp4', 'src':'/styles/fondos/001.mp4'}],
+//       [{type:'mp4', 'src':'/styles/fondos/002.mp4'}],
+//       [{type:'mp4', 'src':'/styles/fondos/003.mp4'}]
+//   ];
   
-  // selecting random item from array,you can make your own
-  var randomitem = videos[Math.floor(Math.random()*videos.length)];
+//   // selecting random item from array,you can make your own
+//   var randomitem = videos[Math.floor(Math.random()*videos.length)];
   
-  // This function adds a new video source (dynamic) in the video html tag
+//   // This function adds a new video source (dynamic) in the video html tag
   
 
-  // this function fires the video for particular video tag
+//   // this function fires the video for particular video tag
   
-  function newvideo(src)
-  {
-  var vid = document.getElementById("video1");
-  videoadd(vid,src ,'video/mp4');
-  vid.autoplay = true;
-  vid.load();
-  //vid.play();
-  }
-  // function call
-  newvideo(randomitem[0].src)
+//   function newvideo(src)
+//   {
+//   var vid = document.getElementById("video1");
+//   videoadd(vid,src ,'video/mp4');
+//   vid.autoplay = true;
+//   vid.load();
+//   //vid.play();
+//   }
+//   // function call
+//   newvideo(randomitem[0].src)
   
-  // Added an event listener so that everytime the video finishes ,a new video is loaded from array
-  document.getElementById('myVideo').addEventListener('ended',handler,false);
+//   // Added an event listener so that everytime the video finishes ,a new video is loaded from array
+//   document.getElementById('myVideo').addEventListener('ended',handler,false);
   
-  function handler(e)
-  {
-  newvideo(randomitem[0].src)
+//   function handler(e)
+//   {
+//   newvideo(randomitem[0].src)
   
-  }
+//   }
     
   
-  })
+ // })
 
 
 
